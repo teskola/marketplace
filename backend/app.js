829c08c634c94abdee4afe0a0319100c06cb1225
teskola <timo.eskola@tuni.fi>
2023-04-13 12:02:00 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const users = require('./routes/users');
-const products = require('./routes/products');
+const express = require("express");
+const cors = require("cors");
+const users = require("./routes/users");
+const products = require("./routes/products");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://final-frontend.onrender.com"],
+  })
+);
 app.use(express.json());
 
-app.use('/api/users', users);
-app.use('/api/products', products);
+app.use("/api/users", users);
+app.use("/api/products", products);
 
-
-app.get('/health', (req, res) => {
-  res.send('OK');
+app.get("/health", (req, res) => {
+  res.send("OK");
 });
 
 module.exports = app;
